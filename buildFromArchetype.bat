1fb66a6ca911a05e3d0ff5126592536cc90b2627
rodoviti <rodolfo.viti@everis.com>
2020-03-28 19:36:54 +0100

    add batch file to generate project from artifact

---
+++ buildFromArchetype.bat
@@ -0,0 +1,10 @@
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+@SET MNAME=test-service
+@SET MNAME_CAMEL=TestService
+@SET MNAME_UPPER=TEST_SERVICE
+@SET MNAME_PACKAGE=test_service
+
+mvn archetype:generate -B -DarchetypeGroupId=it.gov.pagopa.bpd.ms -DarchetypeArtifactId=bpd-ms-archetype -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=it.gov.pagopa.bpd.ms -DartifactId=bpd-ms-%MNAME% -Dversion=1.0-SNAPSHOT -Dpackage=it.gov.pagopa.bpd -DmsName=%MNAME% -DmsNameCamelCase=Bpd%MNAME_CAMEL% -DmsNameUpperCase=BPD_%MNAME_UPPER% -DmsPackageName=%MNAME_PACKAGE%
+
+ENDLOCAL
