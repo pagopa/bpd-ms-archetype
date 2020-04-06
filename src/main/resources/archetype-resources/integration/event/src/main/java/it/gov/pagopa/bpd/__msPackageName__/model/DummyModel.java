654a291a5fc0431fed2d58d8ddbd577cfff9bfef
rodoviti <rodolfo.viti@everis.com>
2020-04-06 22:48:01 +0200

    add jpa test

--- src/main/resources/archetype-resources/integration/event/src/main/java/it/gov/pagopa/bpd/__msPackageName__/model/DummyModel.java
+++ src/main/resources/archetype-resources/integration/event/src/main/java/it/gov/pagopa/bpd/__msPackageName__/model/DummyModel.java
@@ -2,16 +2,14 @@ package ${package}.${msPackageName}.model;
 
 import ${package}.${msPackageName}.model.enums.DummyEnum;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import java.io.Serializable;
 import javax.validation.constraints.NotNull;
 
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data
 //FIXME: remove me (created as archetype test)
 public class DummyModel implements Serializable {
     private String id;
